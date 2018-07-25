ee94678e52f6765b928c3b2230b01a3bfd262848
Yogesh Joshi <yjoshi1984@gmail.com>
2018-07-25 10:39:33 -0700

    Boyer-Moore Algorithm
    Boyer-Moore Algorithm

---
+++ BoyerMoore.py
@@ -0,0 +1,49 @@
+
+# Boyer-Moore Algorithm for the Exact Matching as an application to the Read Alignment Problem.
+# Code Credits: Algorithms for DNA Sequencing Course.
+
+import string
+
+def z_array(s):
+    """ Use Z algorithm (Gusfield theorem 1.4.1) to preprocess s """
+    assert len(s) > 1
+    z = [len(s)] + [0] * (len(s)-1)
+    # Initial comparison of s[1:] with prefix
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            z[1] += 1
+        else:
+            break
+    r, l = 0, 0
+    if z[1] > 0:
+        r, l = z[1], 1
+    for k in range(2, len(s)):
+        assert z[k] == 0
+        if k > r:
+            # Case 1
+            for i in range(k, len(s)):
+                if s[i] == s[i-k]:
+                    z[k] += 1
+                else:
+                    break
+            r, l = k + z[k] - 1, k
+        else:
+            # Case 2
+            # Calculate length of beta
+            nbeta = r - k + 1
+            zkp = z[k - l]
+            if nbeta > zkp:
+                # Case 2a: Zkp wins
+                z[k] = zkp
+            else:
+                # Case 2b: Compare characters just past r
+                nmatch = 0
+                for i in range(r+1, len(s)):
+                    if s[i] == s[i - k]:
+                        nmatch += 1
+                    else:
+                        break
+                l, r = k, r + nmatch
+                z[k] = r - k + 1
+    return z
+
