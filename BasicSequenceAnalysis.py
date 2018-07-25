23d6c1377e8c11a6bd8c6f0d5f37b8b69a8c2717
Yogesh Joshi <yjoshi1984@gmail.com>
2018-07-25 10:23:32 -0700

    Basic Sequence Analysis

--- BasicSequenceAnalysis.py
+++ BasicSequenceAnalysis.py
@@ -25,3 +25,25 @@ def longest_common_prefix(s_1, s_2):
         i += 1
     return s_1[:i]
 
+
+def check_match(s_1, s_2):
+    """ Check if the given sequences are a perfect match. 
+    Python's builtin functionality can also be used.
+    "ATGC" == "ATGC" will return True and likewise. """
+    if not len(s_1) == len(s_2):
+        return False
+    for index, _ in enumerate(s_1):
+        if not s_1[index] == s_2[index]:
+            return False
+    return True
+
+
+def reverse_complement(inseq):
+    """ Return the reverse complement of a given DNA Sequence."""
+    complement_dict = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    rev_comp = ""
+    for base in inseq:
+        rev_comp = complement_dict[base] + rev_comp
+    return rev_comp
+
+
