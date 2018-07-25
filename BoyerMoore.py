a088792f80ed260a7c9d07a610f988a73005867c
Yogesh Joshi <yjoshi1984@gmail.com>
2018-07-25 10:41:28 -0700

    Boyer-Moore Algorithm
    Boyer-Moore Algorithm

--- BoyerMoore.py
+++ BoyerMoore.py
@@ -47,3 +47,43 @@ def z_array(s):
                 z[k] = r - k + 1
     return z
 
+
+def n_array(s):
+    """ Compile the N array (Gusfield theorem 2.2.2) from the Z array """
+    return z_array(s[::-1])[::-1]
+
+
+def big_l_prime_array(p, n):
+    """ Compile L' array (Gusfield theorem 2.2.2) using p and N array.
+        L'[i] = largest index j less than n such that N[j] = |P[i:]| """
+    lp = [0] * len(p)
+    for j in range(len(p)-1):
+        i = len(p) - n[j]
+        if i < len(p):
+            lp[i] = j + 1
+    return lp
+
+
+def big_l_array(p, lp):
+    """ Compile L array (Gusfield theorem 2.2.2) using p and L' array.
+        L[i] = largest index j less than n such that N[j] >= |P[i:]| """
+    l = [0] * len(p)
+    l[1] = lp[1]
+    for i in range(2, len(p)):
+        l[i] = max(l[i-1], lp[i])
+    return l
+
+
+def small_l_prime_array(n):
+    """ Compile lp' array (Gusfield theorem 2.2.4) using N array. """
+    small_lp = [0] * len(n)
+    for i in range(len(n)):
+        if n[i] == i+1:  # prefix matching a suffix
+            small_lp[len(n)-i-1] = i+1
+    for i in range(len(n)-2, -1, -1):  # "smear" them out to the left
+        if small_lp[i] == 0:
+            small_lp[i] = small_lp[i+1]
+    return small_lp
+
+
+
